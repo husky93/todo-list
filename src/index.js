58bb4e6b831f3582fb8d1b2b6173e5937dd07d88
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-15 17:24:09 +0200

    Create Todo factory function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './assets/style.css';
+import Todo from './modules/todo.js';
---
+++ src/modules/todo.js
@@ -0,0 +1,45 @@
+const Todo = (title, description, dueDate, priority) => {
+    let todoTitle = title;
+    let todoDescription = description;
+    let todoDueDate = dueDate;
+    let todoPriority = priority;
+    let isDone = false;
+
+    return {
+        get title() {
+            return todoTitle;
+        },
+        set title(newTitle) {
+            todoTitle = newTitle
+        },
+        get description() {
+            return todoDescription;
+        },
+        set description(newDescription) {
+            todoDescription = newDescription;
+        },
+        get dueDate() {
+            return todoDueDate;
+        },
+        set title(newDueDate) {
+            todoDueDate = newDueDate;
+        },
+        get priority() {
+            return todoPriority;
+        },
+        set title(newPriority) {
+            todoPriority = newPriority;
+        },
+        get isDone() {
+            return isDone;
+        },
+        set isDone(value) {
+            if (typeof(value) === 'boolean') 
+                isDone = value;
+            else
+                throw 'Todo.isDone / Wrong value type provided, must be boolean';
+        }
+    };
+}
+
+export default Todo;
