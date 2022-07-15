3e3167d1e9e1290812e008842341bdd2be803c3e
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-15 16:51:18 +0200

    Fix webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
-  output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        static: './dist',
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
         {
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
