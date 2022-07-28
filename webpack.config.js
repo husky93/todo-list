a6d566ea66925a7a4b73103c95b68258dcc07188
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-28 15:15:32 +0200

    Remove unncecesarry dev tools -reduce size significantly

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devServer: {
-        static: './dist',
-    },
-    devtool: 'inline-source-map',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-    ]
-  }
-}
+  mode: 'production',
+  entry: './src/index.js',
+  // devServer: {
+  //     static: './dist',
+  // },
+  // devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
